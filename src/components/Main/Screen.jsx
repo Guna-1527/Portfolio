1baedddfc7103bdd71bbd613b9c9ba4537a6e28a
Guna-1527 <126180469+Guna-1527@users.noreply.github.com>
2023-11-26 12:22:55 +0530

    Update Screen.jsx

--- src/components/Main/Screen.jsx
+++ src/components/Main/Screen.jsx
@@ -11,9 +11,7 @@ const Screen = () => {
         <p className="Screen__Content__Name">Guna Prasanth</p>
         <p className="Screen__Content__Text">I&apos;m a Full Stack Web Developer</p>
         <p className="Screen__Content__Font">I am a young passionate developer, ready to build enterprise-level applications that help businesses achieve their goals. I am eager to learn and grow, and I am confident that I can make a significant contribution to the assigned projects</p>
-        <div className="Screen__Content__Button">
-        <button className="Screen__Btn">Contact Me<FontAwesomeIcon className="Screen__Icon" icon={faLink} /></button>
-      </div>
+        
       </div>
       <div className="Screen__Image__Container">
         <img className="Screen__Image" src="https://png.pngtree.com/png-vector/20220521/ourmid/pngtree-man-work-computer-deadline-office-png-image_4677851.png" alt="" />
