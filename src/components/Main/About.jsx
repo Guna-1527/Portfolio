737bd499a1fcd765a8703d80992ceb55da6088de
Guna-1527 <126180469+Guna-1527@users.noreply.github.com>
2023-11-28 16:41:29 +0530

    Update About.jsx

--- src/components/Main/About.jsx
+++ src/components/Main/About.jsx
@@ -14,9 +14,9 @@ const About = () => {
             <p className="About__Text__Name">I&apos;m Guna Prasanth</p>
             <p className="About__Text__Profession">I&apos;m a Full Stack Web Developer.</p>
             <p className="About__Text__Description">I&apos;m a Pega and Web Developer who loves to learn new technologies. </p>
-            <p className="Text">Living in Tiruvannamalai. </p>
-            <p className="Text"> Moved to Namakkal for complete my Senior Secondary in SKV Matric Higher secondary school.</p> 
-            <p className="Text">And Currently pursuing my B.E. Computer Science and Engineering bacholar degree in Coimbatore.</p>
+            <p className="About__Text__Description">Living in Tiruvannamalai. </p>
+            <p className="About__Text__Description"> Moved to Namakkal for complete my Senior Secondary in SKV Matric Higher secondary school.</p> 
+            <p className="About__Text__Description">And Currently pursuing my B.E. Computer Science and Engineering bacholar degree in Coimbatore.</p>
           </div>
         </div>
         </div>
