94548ea2fc82f73ff4aaa599c370f81f40625d53
Guna-1527 <126180469+Guna-1527@users.noreply.github.com>
2023-11-28 16:42:45 +0530

    Update About.jsx

--- src/components/Main/About.jsx
+++ src/components/Main/About.jsx
@@ -11,12 +11,12 @@ const About = () => {
         <div className="About__Content">
           
           <div className="About__Content__Text">
-            <p className="About__Text__Name">I&apos;m Guna Prasanth</p>
-            <p className="About__Text__Profession">I&apos;m a Full Stack Web Developer.</p>
-            <p className="About__Text__Description">I&apos;m a Pega and Web Developer who loves to learn new technologies. </p>
-            <p className="About__Text__Description">Living in Tiruvannamalai. </p>
-            <p className="About__Text__Description"> Moved to Namakkal for complete my Senior Secondary in SKV Matric Higher secondary school.</p> 
-            <p className="About__Text__Description">And Currently pursuing my B.E. Computer Science and Engineering bacholar degree in Coimbatore.</p>
+            <p className="About__Text__Name namess">I&apos;m Guna Prasanth</p>
+            <p className="About__Text__Profession namess">I&apos;m a Full Stack Web Developer.</p>
+            <p className="About__Text__Description namess">I&apos;m a Pega and Web Developer who loves to learn new technologies. </p>
+            <p className="About__Text__Description namess">Living in Tiruvannamalai. </p>
+            <p className="About__Text__Descriptionnamess"> Moved to Namakkal for complete my Senior Secondary in SKV Matric Higher secondary school.</p> 
+            <p className="About__Text__Description namess">And Currently pursuing my B.E. Computer Science and Engineering bacholar degree in Coimbatore.</p>
           </div>
         </div>
         </div>
