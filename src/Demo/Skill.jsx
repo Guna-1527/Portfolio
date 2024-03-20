3267fd185b54c4d6636b2ced3b4b3a6f1a82cbe6
Guna-1527 <gunaprasanth1527@gmail.com>
2024-03-20 18:57:42 +0530

    updated

--- src/Demo/Skill.jsx
+++ src/Demo/Skill.jsx
@@ -71,60 +71,66 @@ const Skill = () => {
             >
                 My Skills
             </motion.h1>
-            <div className="relative flex justify-around items-center w-full">
-                <marquee
-                    className="absolute z-[-3] font-bold text-[7rem] text-gray-200"
-                    behavior="Alternate"
-                    direction="left"
-                >
-                    FRONTEND
-                </marquee>
-                {frontend.map((data, i) => (
-                    <div key={i} className="">
-                        <div className="w-[100px] h-[100px] transition-all hover:scale-110 cursor-pointer">
-                            <abbr title={data.skill}>
-                                <motion.img
-                                    initial={{ opacity: 0 }}
-                                    animate={{ opacity: 1 }}
-                                    transition={{
-                                        duration: 1,
-                                        delay: `${data.delay}`,
-                                    }}
-                                    className=""
-                                    src={data.image}
-                                    alt=""
-                                />
-                            </abbr>
-                        </div>
+            <div className="flex gap-20">
+                <div className="relative flex justify-around items-center w-full">
+                    <marquee
+                        className="absolute z-[-3] font-bold text-[7rem] text-gray-200 hidden sm:flex"
+                        behavior="Alternate"
+                        direction="left"
+                    >
+                        FRONTEND
+                    </marquee>
+                    <div className="flex flex-col sm:flex gap-12">
+                        {frontend.map((data, i) => (
+                            <div key={i}>
+                                <div className="w-[100px] h-[100px] transition-all hover:scale-110 cursor-pointer">
+                                    <abbr title={data.skill}>
+                                        <motion.img
+                                            initial={{ opacity: 0 }}
+                                            animate={{ opacity: 1 }}
+                                            transition={{
+                                                duration: 1,
+                                                delay: `${data.delay}`,
+                                            }}
+                                            className=""
+                                            src={data.image}
+                                            alt=""
+                                        />
+                                    </abbr>
+                                </div>
+                            </div>
+                        ))}
                     </div>
-                ))}
-            </div>
-            <div className="relative flex justify-around items-center w-full mt-[70px]">
-                <marquee
-                    className="absolute z-[-3] font-bold text-[7rem] text-gray-200"
-                    behavior="Alternate"
-                    direction="right"
-                >
-                    BACKEND
-                </marquee>
-                {backend.map((data, i) => (
-                    <div key={i} className="w-[100px] h-[100px]">
-                        <div className="w-[100px] transition-all hover:scale-110 h-[100px] cursor-pointer">
-                            <abbr title={data.skill}>
-                                <motion.img
-                                    initial={{ opacity: 0 }}
-                                    animate={{ opacity: 1 }}
-                                    transition={{
-                                        duration: 1,
-                                        delay: `${data.delay}`,
-                                    }}
-                                    src={data.image}
-                                    alt=""
-                                />
-                            </abbr>
-                        </div>
+                </div>
+                <div className="relative flex justify-around items-center w-full mt-[70px]">
+                    <marquee
+                        className="absolute z-[-3] font-bold text-[7rem] text-gray-200 hidden sm:flex"
+                        behavior="Alternate"
+                        direction="right"
+                    >
+                        BACKEND
+                    </marquee>
+                    <div className="flex flex-col gap-12 sm:flex">
+                        {backend.map((data, i) => (
+                            <div key={i} className="w-[100px] h-[100px]">
+                                <div className="w-[100px] transition-all hover:scale-110 h-[100px] cursor-pointer">
+                                    <abbr title={data.skill}>
+                                        <motion.img
+                                            initial={{ opacity: 0 }}
+                                            animate={{ opacity: 1 }}
+                                            transition={{
+                                                duration: 1,
+                                                delay: `${data.delay}`,
+                                            }}
+                                            src={data.image}
+                                            alt=""
+                                        />
+                                    </abbr>
+                                </div>
+                            </div>
+                        ))}
                     </div>
-                ))}
+                </div>
             </div>
             <motion.div className="absolute bottom-[40px] right-[50px] text-black font-bold hover:border-b-2 transition-all ease-in">
                 <Link to="/project">Projects</Link>
